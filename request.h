b28245ed075ecffcfbdd5cc9c3d5295b579dd98f
Kiuliumov <miti.bania@gmail.com>
2026-02-11 14:11:45 +0200

    Update request.h

--- request.h
+++ request.h
@@ -1,38 +1,42 @@
 #ifndef REQUEST_H
 #define REQUEST_H
 
-#define MAX_METHOD_LEN 8
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
 #define MAX_PATH_LEN 256
 #define MAX_VERSION_LEN 16
 
-// HTTP methods
-#define GET_METHOD "GET"
-#define POST_METHOD "POST"
-#define DELETE_METHOD "DELETE"
-#define PUT_METHOD "PUT"
-#define HEAD_METHOD "HEAD"
-#define OPTIONS_METHOD "OPTIONS"
-#define PATCH_METHOD "PATCH"
-#define CONNECT_METHOD "CONNECT"
-#define TRACE_METHOD "TRACE"
+    typedef enum
+    {
+        HTTP_GET,
+        HTTP_POST,
+        HTTP_DELETE,
+        HTTP_PUT,
+        HTTP_HEAD,
+        HTTP_OPTIONS,
+        HTTP_PATCH,
+        HTTP_CONNECT,
+        HTTP_TRACE,
+        HTTP_UNKNOWN
+    } HttpMethod;
 
+    typedef struct
+    {
+        HttpMethod method;
+        char path[MAX_PATH_LEN];
+        char version[MAX_VERSION_LEN];
+    } HttpRequest;
 
+    HttpMethod parse_method(const char *method_str);
 
-typedef struct
-{
-    char method[MAX_METHOD_LEN];
-    char path[MAX_PATH_LEN];
-    char version[MAX_VERSION_LEN];
-} HttpRequest;
+    const char *method_to_string(HttpMethod method);
+    int parse_http_request(const char *buffer, HttpRequest *req);
 
-/**
- * Parses the HTTP request line.
- * Example:
- *   GET /index.html HTTP/1.1
- *
- * Returns 0 on success, -1 on failure.
- */
-
-int parse_http_request(const char *buffer, HttpRequest *req);
+#ifdef __cplusplus
+}
+#endif
 
 #endif
